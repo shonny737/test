32b0525de4133e754fa5dc7a12ce5fa169e2895d
shonny737 <shonny737@gmail.com>
2023-05-24 15:09:02 +0300

    del

--- src/Lesson14/OrderProcessingException.java
+++
@@ -1,7 +0,0 @@
-package Lesson14;
-
-public class OrderProcessingException extends Exception {
-    public OrderProcessingException(String message, Throwable cause) {
-        super(message, cause);
-    }
-}
